c4016a3be6814717a21dd2fb77feccb5963d81b1
Woodsst <amper.sil.tok@gmail.com>
2023-05-28 10:28:56 +0500

    separate menu buttons and added button for back in menu

--- bot/bot.py
+++ bot/bot.py
@@ -4,14 +4,15 @@ import telebot
 from telebot import types
 from telebot.types import Message
 
+from bot.menu import menu
 from bot.resin.resin_counter import User
 from log import logger
 from users import users
 
-bot = telebot.TeleBot(getenv("TOKEN"))
+bot = telebot.TeleBot(getenv("TOKEN", "6290030549:AAF9pOX40vz4bW6NfkZKL3nki8X74YtdpTA"))
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["menu", "start"])
 def start(message: Message):
     """Основное меню."""
     user = User(message.chat.id, message.from_user.username)
@@ -19,11 +20,7 @@ def start(message: Message):
     if user_in_work is None:
         users[user.id] = user
         logger.info(f"add user {user.id, user.name}\nusers count = {len(users)}")
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn_resin = types.KeyboardButton("Смола")
-    btn_treasure_resident = types.KeyboardButton("Сокровища обители")
-    btn_parameter_converter = types.KeyboardButton("Параметрический преобразователь")
-    markup.add(btn_resin, btn_parameter_converter, btn_treasure_resident)
-    bot.send_message(message.chat.id,
-                     text="Выберите необходимые данные".format(
-                         message.from_user), reply_markup=markup)
+    menu(message, bot)
+    if message.text == "/start":
+        bot.send_message(message.chat.id, "Привет, это бот помошник для Геншина, "
+                                          "тут ты сможешь задавать и отслеживать состояния своих сокровищ")
--- bot/resin/resin_handler.py
+++ bot/resin/resin_handler.py
@@ -4,6 +4,7 @@ from telebot import types
 from telebot.types import Message
 
 from bot.bot import bot
+from bot.menu import menu
 from users import users
 from bot.resin.resin_counter import User
 from log import logger
@@ -13,16 +14,19 @@ def resin_buttons(message: Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_resin = types.KeyboardButton("Кол-во смолы")
     btn_treasure_resident = types.KeyboardButton("Задать значение")
-    markup.add(btn_resin, btn_treasure_resident)
+    btn_menu = types.KeyboardButton("Вернуться в меню")
+    markup.add(btn_menu, btn_resin, btn_treasure_resident)
     bot.send_message(message.chat.id,
                      text="Выберите необходимые данные".format(
                          message.from_user), reply_markup=markup)
 
 
 @bot.message_handler()
-def take_resin(message: Message):
+def resin_buttons_work(message: Message):
     logger.info(f"user {message.from_user.username} send request - {message.text}")
     user: Optional[User] = users.get(message.chat.id)
+    if message.text == "Вернуться в меню":
+        menu(message, bot)
     if message.text == "Задать значение":
         bot.send_message(chat_id=message.chat.id, text="Введите - s <кол-во смолы>")
         user.resin_counter_thread = False
@@ -37,3 +41,5 @@ def take_resin(message: Message):
         if user.resin_counter_thread is True:
             user.resin_counter_thread = False
         user.resin_thread_start(resin, bot, message)
+    if message.text == "Кол-во смолы":
+        bot.send_message(chat_id=message.chat.id, text=user.resin)
