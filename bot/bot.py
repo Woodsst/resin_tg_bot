eae582dad9571c1ecad6597a9ea2676a19cc6cab
Woodsst <amper.sil.tok@gmail.com>
2023-06-01 15:37:31 +0500

    switch to async

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from bot.menu import menu
@@ -8,13 +8,14 @@ from bot.resin.resin_counter import User
 from log import logger
 from users import users
 
-bot = telebot.TeleBot(
+
+bot = AsyncTeleBot(
     getenv("TOKEN")
 )
 
 
 @bot.message_handler(commands=["Меню", "start"])
-def start(message: Message):
+async def start(message: Message):
     """Основное меню."""
     user = User(message.chat.id, message.from_user.username)
     user_in_work: User = users.get(user.id)
@@ -23,9 +24,9 @@ def start(message: Message):
         logger.info(
             f"add user {user.id, user.name}\nusers count = {len(users)}"
         )
-    menu(message, bot)
+    await menu(message, bot)
     if message.text == "/start":
-        bot.send_message(
+        await bot.send_message(
             message.chat.id,
             "Привет, это бот помошник для Геншина, "
             "тут ты сможешь задавать и отслеживать состояния своих сокровищ",
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bot.bot import bot
 from bot.resin import resin_handler
 from log import logger
+from asyncio import run
 
 if __name__ == "__main__":
     logger.info("start")
-    bot.infinity_polling()
+    run(bot.infinity_polling())
