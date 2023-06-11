1bae7a2ba4421425122447837e8efa6c62c0840f
Woodsst <amper.sil.tok@gmail.com>
2023-06-11 12:39:40 +0500

    initial storage structure

---
+++ storage/storage.py
@@ -0,0 +1,18 @@
+class Storage:
+    def __init__(self):
+        pass
+
+    def add_user(self):
+        pass
+
+    def add_resin_data(self):
+        pass
+
+    def add_treasure_data(self):
+        pass
+
+    def add_converter_data(self):
+        pass
+
+    def get_user_data(self):
+        pass
